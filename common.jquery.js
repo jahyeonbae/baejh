df1bee18c6a1b308daaf76e0fac10e3b82faf8bc
jahyeon <siamsunset13@naver.com>
2017-12-14 12:45:05 +0900

    a

---
+++ common.jquery.js
@@ -0,0 +1,20 @@
+// $(function(){
+//     var count=0
+//     function play(){
+//         msg=typeMsg.innerText
+//         comp=msg.length
+//         type()
+// }
+//     function type(){
+//         if(count<=comp){
+//             typeMsg.innerText=msg.substring(0,cont)
+//             count++
+//             setTimeout("type()",200)
+//         }
+//         else{
+//             count=0
+//             play()
+//         }
+//     }
+// }
+   
